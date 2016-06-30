f1fd65c90375cec20e9c38799e42525114d61017
Steven De Oliveira <de.oliveira.steven@gmail.com>
2016-06-30 16:49:23 +0200

    adding test c file for nested loop

---
+++ test/petter_opt.c
@@ -0,0 +1,29 @@
+int petter(int N){
+  int x = 0, y = 0,a,b,z;
+  
+  while(y < N)
+    {
+      a = y;
+      b = y;
+      z = 0;
+      while (a != 0){
+	if ( a % 2 ==1 )
+	{
+	  z = z+b;
+	  a = a-1;
+	}
+      b = 2*b;
+      a = a/2;
+      }
+      x += z
+      
+      y++;
+    }
+  y--;
+  return x;
+}
+
+int main(){
+  petter(12);
+
+}
