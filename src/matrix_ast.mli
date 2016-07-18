bd2dee7efe4345fbb12c06eedee7938bae57a8af
Steven De Oliveira <de.oliveira.steven@gmail.com>
2016-07-18 17:04:20 +0200

    Adding headers

--- src/matrix_ast.mli
+++ src/matrix_ast.mli
@@ -1,3 +1,25 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  This file is part of Frama-C.                                         *)
+(*                                                                        *)
+(*  Copyright (C) 2007-2016                                               *)
+(*    CEA (Commissariat à l'énergie atomique et aux énergies              *)
+(*         alternatives)                                                  *)
+(*                                                                        *)
+(*  you can redistribute it and/or modify it under the terms of the GNU   *)
+(*  Lesser General Public License as published by the Free Software       *)
+(*  Foundation, version 3.                                                *)
+(*                                                                        *)
+(*  It is distributed in the hope that it will be useful,                 *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *)
+(*  GNU Lesser General Public License for more details.                   *)
+(*                                                                        *)
+(*  See the GNU Lesser General Public License version 3                   *)
+(*  for more details (enclosed in the file LICENCE).                      *)
+(*                                                                        *)
+(**************************************************************************)
+
 open Pilat_matrix
 open Poly_affect 
 open Cil_datatype
