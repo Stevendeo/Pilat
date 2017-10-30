05d1ca9c254ed765321bb2dd747c9e046ae97495
Steven De Oliveira <de.oliveira.steven@gmail.com>
2017-10-30 16:14:17 +0100

    merging simple changes

--- test/fibonacci.c
+++ test/fibonacci.c
@@ -1,10 +1,15 @@
+#include <stdio.h>
+
 int main(){
-  int xn1, xn;
-  while (1)
+  int xn1 = 0, xn = 1, N = 0;
+  while (N < 30)
     {
+      N ++;
       xn1 = xn1 + xn;
       xn = xn1 - xn;
     }
+
+  printf("%i", xn);
   return 0;
 
 }
